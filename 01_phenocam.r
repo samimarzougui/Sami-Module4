0ce2d6e4e4302769d3b564ecd17388d66addccf8
Your Name <samimarzougui@yahoo.com>
2021-03-11 20:05:55 +0000

    message

---
+++ 01_phenocam.r
@@ -0,0 +1,15 @@
+{
+  ## check that we've been passed a URL
+  if (length(URL) == 1 & is.character(URL) & substr(URL, 1, 4) == "http") {
+    
+    ## read data
+    dat <- read_csv(URL, skip = 22)
+    
+    ## convert date
+    dat$date <- as_date(as.character(dat$date))
+    
+    return(dat)
+  } else {
+    print(paste("download.phenocam: Input URL not provided correctly", URL))
+  }
+}
